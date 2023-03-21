48184eb66030f957c8222937c2c00c8e744244b8
dbswp <dbswp980427@gmail.com>
2023-03-21 22:18:03 +0900

    ejs 파일 생성 및 login.js 작성

---
+++ router/routes/login.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userDB = require('../../controllers/userControllers');
+
+const router = express.Router();
+
+// local:4000이 요청되면 login 페이지 실행
+
+router.post('/', async (req, res) => {});
