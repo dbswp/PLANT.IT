bcaa0d84f84f1d6dba29533dd5dd3e721d8b2ab3
ssb1565b <ssongsumovie@naver.com>
2023-03-21 23:05:08 +0900

    feat[#9] 회원가입

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const userDB = require('../../controllers/userControllers');
+const userDB = require('../controllers/userControllers');
 const fs = require('fs');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  await res.render('login');
+});
 // local:4000이 요청되면 login 페이지 실행
-
 router.post('/', async (req, res) => {
   const duplicatedUser = await userDB.userCheck(req.body.id);
   if (!duplicatedUser) {
@@ -18,3 +20,4 @@ router.post('/', async (req, res) => {
     );
   }
 });
+module.exports = router;
