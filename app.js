3e8083d16b9ee1eb1029fd0b83e11b3191d353fe
dbswp <dbswp980427@gmail.com>
2023-03-21 21:34:16 +0900

    express 설정 및 ejs파일 생성

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = 4000;
+
+const mainRouter = require('./router/routes/index');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.use(express.static('pulic'));
+app.use('/', mainRouter);
+
+app.use('/', (req, res) => {
+  res.send('연결 성공');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(err.statusCode);
+  res.send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} 번에서 실행 중`);
+});
